3c7912f5115e2a81f66b338088984087f810201a
ParamBirje <parambirje@gmail.com>
2024-07-07 17:37:50 +0530

    Added image hover functionality on featured projects

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
-let scroll = new LocomotiveScroll({
-  el: document.querySelector("main"),
-  smooth: true,
+// let scroll = new LocomotiveScroll({
+//   el: document.querySelector("main"),
+//   smooth: true,
+// });
+
+let fixedImage = document.querySelector("#fixed_image");
+
+// Adding hover listeners to all the project items
+let projectItems = document.querySelectorAll(".element");
+projectItems.forEach((item) => {
+  let currentImage = item.getAttribute("data-image");
+
+  item.addEventListener("mouseenter", function () {
+    fixedImage.style.display = "block";
+    fixedImage.style.backgroundImage = `url(${currentImage})`;
+  });
+
+  item.addEventListener("mouseleave", function () {
+    fixedImage.style.display = "none";
+  });
 });
